dd5aa62a0408a0c942a90ddb5540c6326ca1a20e
Nick S. B. <nick-s-b@users.noreply.github.com>
2026-05-29 11:38:15 -0400

    Merge pull request #6 from KalleLeskinen/fix-desuarchive-404
    Fix Desuarchive image redirects

--- src/Archive/Redirect.ts
+++ src/Archive/Redirect.ts
@@ -191,7 +191,7 @@ var Redirect = {
     } else {
       if (/[sm]\.jpg$/.test(filename)) { return ''; }
     }
-    if (archive.name.endsWith('arch.b4k.co')) {
+    if (archive.name.endsWith('arch.b4k.co') || archive.name.endsWith('Desuarchive')) {
       const [timeStamp, ext] = filename.split('.');
       if (timeStamp.length > 13) {
         // remove last 3 digits
