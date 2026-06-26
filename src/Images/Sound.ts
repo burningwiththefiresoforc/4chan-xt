306ee26f82e35f163df3617d5b9134ba2d7b2978
burningwiththefiresoforc <burningwiththefiresoforc@github.com>
2026-06-25 22:18:19 -0700

    Safer checking for soundposts

--- src/Images/Sound.ts
+++ src/Images/Sound.ts
@@ -36,15 +36,17 @@ const Sound = {
   },
 
   setupSoundpost(el: HTMLElement, file: File) {
-    const soundUrlMatch = file.name.match(/\[sound=([^\]]+)]/i);
+    const soundUrlMatch = file.name.match(/\[sound=([^\]]+\.(?:webm|ogg|mp3|wav|m4a))]/i);
     if (!soundUrlMatch) return;
 
-    let src = decodeURIComponent(soundUrlMatch[1]);
-    if (!src.startsWith('http')) {
-      src = `https://${src}`;
-    }
+    let url: URL;
+    try {
+      const src = decodeURIComponent(soundUrlMatch[1]);
+      url = new URL(src.startsWith('http') ? src : `https://${src}`);
+    } catch { return; }
 
-    const audioEl = new Audio(src);
+    const audioEl = new Audio(url.href);
+
     Volume.setup(audioEl);
     if (el instanceof HTMLVideoElement) {
       Sound.setupSync(el, audioEl);
@@ -53,10 +55,9 @@ const Sound = {
     audioEl.loop = true;
     audioEl.controls = Conf['Show Controls'];
     audioEl.autoplay = Conf.Autoplay;
-
     el.after(audioEl);
     file.audio = audioEl;
   }
-};
+}
 
 export default Sound;
